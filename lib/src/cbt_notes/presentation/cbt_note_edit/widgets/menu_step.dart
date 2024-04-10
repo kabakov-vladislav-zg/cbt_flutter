954ff2c59bab30bf7c8bea18ea49a106cafa2be0
vladislav <kabakov.vladislav@adinadin.ru>
2024-04-10 12:47:02 +0400

    feat: edit steps

--- lib/src/cbt_notes/presentation/cbt_note_edit/widgets/menu_step.dart
+++ lib/src/cbt_notes/presentation/cbt_note_edit/widgets/menu_step.dart
@@ -3,32 +3,52 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../bloc/cbt_note_edit_cubit.dart';
 
-class MenuStep extends StatelessWidget {
+class MenuStep extends StatefulWidget {
   const MenuStep({ super.key });
 
   @override
+  State<MenuStep> createState() => _MenuStepState();
+}
+class _MenuStepState extends State<MenuStep> {
+  late final CbtNoteEditCubit _cubit;
+  List<DropdownMenuItem<EditStep>> _dropdownMenuItems = [];
+  late EditStep _editStep;
+
+  @override
+  void initState() {
+    _cubit = context.read<CbtNoteEditCubit>();
+    _setEntries();
+    super.initState();
+  }
+
+  _setEntries() {
+    final avaibleSteps = _cubit.getAvaibleSteps();
+    setState(() {
+      _dropdownMenuItems = avaibleSteps.map((EditStep editStep) {
+        return DropdownMenuItem<EditStep>(
+          value: editStep,
+          child: Text(editStep.name),
+        );
+      }).toList();
+      _editStep = _cubit.state.editStep;
+    });
+  }
+
+  void _onChanged(EditStep? editStep) {
+    if (editStep == null || editStep == _editStep) return;
+    _cubit.setStep(editStep);
+  }
+  
+  @override
   Widget build(BuildContext context) {
-    return BlocSelector<CbtNoteEditCubit, CbtNoteEditState, EditStep>(
-      selector: (state) => state.editStep,
-      builder: (BuildContext context, EditStep editStep) {
-        return editStep == EditStep.creation
-          ? const Text('')
-          : SizedBox(
-            width: 56,
-            height: 56,
-            child: IconButton(
-              onPressed: () {
-                final cubit = context.read<CbtNoteEditCubit>();
-                if (editStep == EditStep.viewing) {
-                  cubit.setStep(EditStep.deconstruction);
-                } else {
-                  cubit.setStep(EditStep.creation);
-                }
-              },
-              icon: const Icon(Icons.edit),
-            ),
-          );
-      }
+    return BlocListener<CbtNoteEditCubit, CbtNoteEditState>(
+      listener: (context, state) => _setEntries(),
+      listenWhen: (previous, current) => previous.editStep != current.editStep,
+      child: DropdownButton<EditStep>(
+        value: _editStep,
+        onChanged: _onChanged,
+        items: _dropdownMenuItems,
+      ),
     );
   }
 }
